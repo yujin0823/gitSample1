a9e4c216246f80b573fd54ca0390681c562e4baf
lctumaca1 <coth0524@gmail.com>
2019-09-25 16:30:28 +0900

    modify

--- src/main/MainApp.java
+++ src/main/MainApp.java
@@ -3,5 +3,6 @@ package main;
 public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!!!!!!!!!");
+		System.out.println("abdjkfhjkdhnfjkhkj");
 	}
 }
